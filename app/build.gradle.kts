8eacebbd15fe441f013e7b11e21d96f1bdb9ce6d
Dream-City6 <102643176+Dream-City6@users.noreply.github.com>
2026-08-29 09:09:59 +0900

    chore: bump integrated migration beta8 version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.alex.speedshare"
         minSdk = 26
         targetSdk = 36
-        versionCode = 29
-        versionName = "1.5.0-beta7"
+        versionCode = 30
+        versionName = "1.5.0-beta8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
